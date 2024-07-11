8fa5cc7d7d37136b512b25e9835c9be1a4c4f9c6
guille_medina575@hotmail.com <guille_medina575@hotmail.com>
2024-07-10 23:31:08 -0600

    elementos nuevos

---
+++ TowerDefense/Assets/_Scripts/GameElements/Towers/_Tower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class _Tower : MonoBehaviour
+{
+    public _Enemy enemigo;
+    public GameObject prefabbala;
+    public List<GameObject> puntasCanon;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (enemigo != null)
+        {
+            Apuntar();
+        }
+    }
+    public void Apuntar()
+    {
+        transform.LookAt(enemigo.transform);
+    }
+    public virtual void Disparar()
+    {
+        foreach(GameObject punta in puntasCanon)
+        {
+            var tempBala = Instantiate<GameObject>(prefabbala, punta.transform.position, Quaternion.identity);
+            tempBala.GetComponent<Bala>().destino = enemigo.transform.position;
+        }
+    }
+}
