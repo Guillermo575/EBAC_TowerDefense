e91d67c0a9728b6ff8f56a6620d1eeeb96e69fe1
guille_medina575@hotmail.com <guille_medina575@hotmail.com>
2024-07-14 00:19:01 -0600

    torre 3

---
+++ TowerDefense/Assets/_Scripts/GameElements/Towers/TowerAntena.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TowerAntena : _Tower, IAtacante
+{
+    #region Variables
+    public float divisionesRayo = 10;
+    private LineRenderer LRRayo;
+    public List<Vector3> puntos;
+    public int potenciaRayo;
+    public MathRNG mathRNG = new MathRNG(1121);
+    #endregion
+
+    public void Start()
+    {
+        LRRayo = GetComponent<LineRenderer>();
+    }
+    private void FixedUpdate()
+    {
+        if (enemigo != null)
+        {
+            Disparar();
+            MakeDamage(potenciaRayo);
+        }
+        else
+        {
+            LRRayo.positionCount = 0;
+        }
+    }
+
+    public override void Disparar()
+    {
+        puntos = ObtenerPuntos();
+        puntos.Insert(0, puntasCanon[0].transform.position);
+        var posEnemigo = enemigo.transform.position;
+        posEnemigo.y += 1;
+        puntos.Add(posEnemigo);
+        LRRayo.positionCount = puntos.Count;
+        LRRayo.SetPositions(puntos.ToArray());
+    }
+
+    private List<Vector3> ObtenerPuntos()
+    {
+        if (divisionesRayo == 0)
+        {
+            return null;
+        }
+        List<Vector3> puntosTemporales = new List<Vector3>();
+        float divisor = 1f / divisionesRayo;
+        float linear = 0f;
+        bool esPositivo = false;
+        if (divisionesRayo == 1)
+        {
+            var punto = Vector3.Lerp(puntasCanon[0].transform.position, enemigo.transform.position, 0.5f);
+            puntosTemporales.Add(punto);
+            return puntosTemporales;
+        }
+        for (int l = 0; l < divisionesRayo; l++)
+        {
+            if (l == 0)
+            {
+                linear = divisor / 2;
+            }
+            else
+            {
+                linear += divisor;
+            }
+            var punto = Vector3.Lerp(puntasCanon[0].transform.position, enemigo.transform.position, linear);
+            if (esPositivo)
+            {
+                punto.x += (float)(mathRNG.GetRandom() * 2);
+                esPositivo = false;
+            }
+            else
+            {
+                punto.x -= (float)(mathRNG.GetRandom() * 2);
+                esPositivo = true;
+            }
+            puntosTemporales.Add(punto);
+        }
+        return puntosTemporales;
+    }
+
+    public void MakeDamage(int damage = 0)
+    {
+        enemigo.GetComponent<_Enemy>().ReceiveDamage(damage);
+    }
+}
